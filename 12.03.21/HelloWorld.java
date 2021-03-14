1fe2e90c5267bf509426a26e755b7cf4fb0c0669
Sunsh4rd <SsoslickK@gmail.com>
2021-03-14 13:44:16 +0400

    -d ./ -cp ./

--- 12.03.21/HelloWorld.java
+++ 12.03.21/HelloWorld.java
@@ -1,7 +1,9 @@
 package asd;
+import some.*;
 
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		Some.yya();
 	}
 }
---
+++ 12.03.21/Some.java
@@ -0,0 +1,7 @@
+package some;
+
+public class Some {
+	public static void yya(	) {
+		System.out.println("yya");
+	}
+}
