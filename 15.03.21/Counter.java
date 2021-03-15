09b401404a7cd369092d9823fd2699efa83af407
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-03-15 19:13:34 +0400

    Update Counter.java

--- 15.03.21/Counter.java
+++ 15.03.21/Counter.java
@@ -1,12 +1,23 @@
 public class Counter {
 	static int countExecutions;
+	static String counterPhrase;
+
+	static {
+		countExecutions = 0;
+		counterPhrase = "Method Executed";
+	}
 	public static void doSomeStuff() {
-		System.out.println("Method executed.");
+		System.out.println(Counter.counterPhrase);
 		countExecutions++;
-		System.out.println(countExecutions);
+		System.out.println(Counter.countExecutions);
+	}
+	public Counter(String phrase) {
+		this.counterPhrase = phrase;
 	}
 	public static void main(String[] args) {
 		Counter.doSomeStuff();
+		Counter c = new Counter("Now another phrase...");
+		c.doSomeStuff();
 		Counter.doSomeStuff();
 	}
 }
