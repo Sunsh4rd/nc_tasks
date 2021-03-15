aa878e0fb7d1886f86a61595d3b8a20cb6ac46a1
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-03-15 20:04:20 +0400

    Update IntegersComp.java

--- 15.03.21/IntegersComp.java
+++ 15.03.21/IntegersComp.java
@@ -2,13 +2,11 @@ public class IntegersComp {
 
 	public static void main(String[] args) {
 		Integer construct = new Integer(5);
-		int autounboxed = construct;
-		int mixed = construct + autounboxed;
-		System.out.println(mixed);
+		Integer construct1 = new Integer(5);
 
 		Integer autoboxed = 10;
-		if (autoboxed.equals(mixed)) {
-			System.out.println("Yeah...");
-		}
+		Integer autoboxed1 = 10;
+		System.out.println(construct == construct1);
+		System.out.println(autoboxed == autoboxed1);
 	}
 }
