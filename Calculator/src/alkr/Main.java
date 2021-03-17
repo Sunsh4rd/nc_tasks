773ec58d978950b0d88fe194d1c5253f05f2074b
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-03-17 22:03:22 +0400

    Commit?

--- Calculator/src/alkr/Main.java
+++ Calculator/src/alkr/Main.java
@@ -2,7 +2,12 @@ package alkr;
 
 public class Main {
 
+    public static void justToCommit() {
+        System.out.println("Commit?");
+    }
+
     public static void main(String[] args) {
         System.out.println("Initial file of calc...");
+        justToCommit();
     }
 }
