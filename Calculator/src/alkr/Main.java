a4dd60bb31e873a1af3cad150591bf827badcdb9
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-03-18 13:36:02 +0400

    Update Main.java

--- Calculator/src/alkr/Main.java
+++ Calculator/src/alkr/Main.java
@@ -3,6 +3,6 @@ package alkr;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Commit w/o bullshit");
+        System.out.println("Commit w/o stuff");
     }
 }
