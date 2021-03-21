833e31d6770b563956ac7105230e7776c19dc5a1
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-03-22 02:38:01 +0400

    Calculator

--- Calculator/src/alkr/Main.java
+++ Calculator/src/alkr/Main.java
@@ -6,5 +6,12 @@ public class Main {
     public static void main(String[] args) {
 	    Calculator calc = new Calculator("Calculator");
         System.out.println(calc.add(5,6));
+        System.out.println(calc.sub(5,6));
+        System.out.println(calc.multi(5,6));
+        System.out.println(calc.divide(5,6));
+        System.out.println(calc.divide(12,2));
+        System.out.println(calc.divide(12,2.));
+        System.out.println(calc.divide(5,0));
+        System.out.println(calc.divide(5,0.));
     }
 }
--- Calculator/src/calc/Calculator.java
+++ Calculator/src/calc/Calculator.java
@@ -8,8 +8,33 @@ public class Calculator {
         this.name = name;
     }
 
-    public int add(int a, int b) {
-        System.out.println(this.name);
+    public double add(double a, double b) {
         return a + b;
+    }
+
+    public double sub(double a, double b) {
+        return a - b;
+    }
+
+    public double multi(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public int divide(int a, int b) {
+        return divideInternal(a,b);
+    }
+
+    private int divideInternal(int a, int b) {
+        try {
+            return a / b;
+        }
+        catch (ArithmeticException e) {
+            System.out.println("a = " + a + "b = " + b + " " + e.getMessage());
+            return Integer.MAX_VALUE;
+        }
     }
 }
