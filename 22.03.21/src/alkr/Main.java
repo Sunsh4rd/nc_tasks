93c8ecf3be3278b6918df0a2d56bce84a477a316
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-03-22 20:04:08 +0400

    Update Main.java

--- 22.03.21/src/alkr/Main.java
+++ 22.03.21/src/alkr/Main.java
@@ -5,22 +5,22 @@ import inter.Movable;
 public class Main {
 
     public static void main(String[] args) {
-//        Parent p = new Parent(3, 8);
-//        Child c = new Child(11, 12);
-//        System.out.println(p.getFieldOne());
-//        System.out.println(p.getFieldTwo());
-//        System.out.println(c.getFieldOne());
-//        System.out.println(c.getFieldTwo());
+        Parent p = new Parent(3, 8);
+        Child c = new Child(11, 12);
+        System.out.println(p.getFieldOne());
+        System.out.println(p.getFieldTwo());
+        System.out.println(c.getFieldOne());
+        System.out.println(c.getFieldTwo());
 
-        Car c = new Car();
-        Aircraft a = new Aircraft();
-        ElectroCar ec = new ElectroCar();
-        c.move();
-        c.refuel();
-        a.move();
-        a.refuel();
-        ec.move();
-        ec.refuel();
+//        Car c = new Car();
+//        Aircraft a = new Aircraft();
+//        ElectroCar ec = new ElectroCar();
+//        c.move();
+//        c.refuel();
+//        a.move();
+//        a.refuel();
+//        ec.move();
+//        ec.refuel();
     }
 }
 
@@ -63,39 +63,39 @@ class ElectroCar extends Car {
     }
 }
 
-//class Parent {
-//
-//    private int fieldOne;
-//    private int fieldTwo;
-//
-//    public Parent(int first, int second) {
-//        this.fieldOne = first;
-//        this.fieldTwo = second;
-//    }
-//
-//    public int getFieldOne() {
-//        return this.fieldOne;
-//    }
-//
-//    public int getFieldTwo() {
-//        return this.fieldTwo;
-//    }
-//}
-//
-//class Child extends Parent {
-//
-//    private int fieldOne;
-//    private int fieldTwo;
-//
-//    public Child(int first, int second) {
-//        super(first, second);
-//    }
-//
-//    @Override
-//    public int getFieldOne() {
-//        return this.fieldOne;
-//    }
-//}
+class Parent {
+
+    private int fieldOne;
+    private int fieldTwo;
+
+    public Parent(int first, int second) {
+        this.fieldOne = first;
+        this.fieldTwo = second;
+    }
+
+    public int getFieldOne() {
+        return this.fieldOne;
+    }
+
+    public int getFieldTwo() {
+        return this.fieldTwo;
+    }
+}
+
+class Child extends Parent {
+
+    private int fieldOne;
+    private int fieldTwo;
+
+    public Child(int first, int second) {
+        super(first, second);
+    }
+
+    @Override
+    public int getFieldOne() {
+        return this.fieldOne;
+    }
+}
 
 //class Earth {
 //
