46dab09cb4d3c295269fb8ab742e811e85d57cca
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-03-29 19:46:25 +0400

    Compare getClass()

--- 29.03.21/src/alkr/Main.java
+++ 29.03.21/src/alkr/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
         System.out.println(s.getClass());
         System.out.println(i.getClass());
+        System.out.println(c.getClass());
 
         System.out.println(s.getClass().equals(c.getClass()));
 
