f21bdad71ce1c6b0ef4fff54a21a289db1e3ac83
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-04-02 18:46:26 +0400

    Humanable compare

--- 02.04.21/src/alkr/Main.java
+++ 02.04.21/src/alkr/Main.java
@@ -7,42 +7,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Pupil> pupils = new ArrayList<>();
-        ArrayList<Student> students = new ArrayList<>();
+        ArrayList<Humanable> learners = new ArrayList<>();
 
         Student student1 = new Student(new int[] {1, 4, 5, 3});
         Student student2 = new Student(new int[] {5, 3});
         Student student3 = new Student(new int[] {2, 2, 2, 2, 2});
         Student student4 = new Student(new int[] {1, 4, 5, 2});
 
-        students.add(student1);
-        students.add(student2);
-        students.add(student3);
-        students.add(student4);
-
-        System.out.println(students);
-        students.sort(((o1, o2) -> o1.compareTo(o2)));
-        System.out.println(students);
+        learners.add(student1);
+        learners.add(student2);
+        learners.add(student3);
+        learners.add(student4);
 
         Pupil pupil1 = new Pupil(new int[] {2, 5, 4});
         Pupil pupil2 = new Pupil(new int[] {3, 5, 5, 5, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5});
         Pupil pupil3 = new Pupil(new int[] {3, 3, 3, 3});
         Pupil pupil4 = new Pupil(new int[] {2, 5, 2, 5, 2, 5});
 
-        pupils.add(pupil1);
-        pupils.add(pupil2);
-        pupils.add(pupil3);
-        pupils.add(pupil4);
+        learners.add(pupil1);
+        learners.add(pupil2);
+        learners.add(pupil3);
+        learners.add(pupil4);
 
-        System.out.println(pupils);
-        pupils.sort(Pupil::compareTo);
-        System.out.println(pupils);
+        System.out.println(learners);
+        learners.sort((Comparable::compareTo));
+        System.out.println(learners);
     }
 }
 
-interface Humanable { }
+interface Humanable extends  Comparable <Humanable> {
 
-class Student implements Humanable, Comparable<Student> {
+    int marksSum();
+}
+
+class Student implements Humanable {
 
     private int[] marks = new int[100];
 
@@ -58,7 +56,20 @@ class Student implements Humanable, Comparable<Student> {
         this.marks = marks;
     }
 
-    private int marksSum() {
+    @Override
+    public String toString() {
+        return "Student{" +
+                "marks=" + Arrays.toString(marks) +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Humanable o) {
+       return this.marksSum() - o.marksSum();
+    }
+
+    @Override
+    public int marksSum() {
         int sum = 0;
         for (int i = 0; i < marks.length; i++) {
             if (marks[i] != 0) {
@@ -67,21 +78,9 @@ class Student implements Humanable, Comparable<Student> {
         }
         return sum;
     }
-
-    @Override
-    public int compareTo(Student o) {
-        return this.marksSum() - o.marksSum();
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "marks=" + Arrays.toString(marks) +
-                '}';
-    }
 }
 
-class Pupil implements Humanable, Comparable<Pupil> {
+class Pupil implements Humanable {
 
     private int[] marks = new int[100];
 
@@ -97,7 +96,15 @@ class Pupil implements Humanable, Comparable<Pupil> {
         this.marks = marks;
     }
 
-    private int marksSum() {
+    @Override
+    public String toString() {
+        return "Pupil{" +
+                "marks=" + Arrays.toString(marks) +
+                '}';
+    }
+
+    @Override
+    public int marksSum() {
         int sum = 0;
         for (int i = 0; i < marks.length; i++) {
             if (marks[i] == 5) {
@@ -108,14 +115,7 @@ class Pupil implements Humanable, Comparable<Pupil> {
     }
 
     @Override
-    public String toString() {
-        return "Pupil{" +
-                "marks=" + Arrays.toString(marks) +
-                '}';
-    }
-
-    @Override
-    public int compareTo(Pupil o) {
+    public int compareTo(Humanable o) {
         return this.marksSum() - o.marksSum();
     }
 }
