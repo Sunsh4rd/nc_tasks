f726f6e851a355ebad3550523315b05677bd2a89
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-04-05 03:25:29 +0400

    try to connect

--- TestSpringInitializrInIdea/src/main/java/com/example/demo/TestSpringInitializrInIdeaApplication.java
+++ TestSpringInitializrInIdea/src/main/java/com/example/demo/TestSpringInitializrInIdeaApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
 import hello.HelloWorld;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
-public class TestSpringInitializrInIdeaApplication {
+public class TestSpringInitializrInIdeaApplication implements CommandLineRunner {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
 
@@ -20,4 +28,12 @@ public class TestSpringInitializrInIdeaApplication {
         Arrays.asList(names).forEach(System.out::println);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        String sql = "SELECT * FROM Patients";
+        List<Patient> customers = jdbcTemplate.query(sql,
+                BeanPropertyRowMapper.newInstance(Patient.class));
+
+        customers.forEach(System.out :: println);
+    }
 }
