11088d43798ab4a85a1d35fe474fcd146cce48de
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-04-09 17:17:07 +0400

    changes with annotations

--- TaxiStation/src/main/java/com/main/taxistation/controller/TaxiStationController.java
+++ TaxiStation/src/main/java/com/main/taxistation/controller/TaxiStationController.java
@@ -1,21 +1,22 @@
 package com.main.taxistation.controller;
 
 import com.main.taxistation.model.CarRepository;
-import com.main.taxistation.model.cars.Car;
 import com.main.taxistation.model.cars.passenger.PassengerCar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path = "/taxistation")
 public class TaxiStationController {
 
-    @Autowired
-    private CarRepository carRepository;
+    private final CarRepository carRepository;
+
+    public TaxiStationController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
 
     @PostMapping(path="/add")
     public @ResponseBody String addNewCar (@RequestParam String brand
--- TaxiStation/src/main/java/com/main/taxistation/model/CarRepository.java
+++ TaxiStation/src/main/java/com/main/taxistation/model/CarRepository.java
@@ -2,7 +2,9 @@ package com.main.taxistation.model;
 
 import com.main.taxistation.model.cars.Car;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends CrudRepository<Car, Integer> {
 
 }
