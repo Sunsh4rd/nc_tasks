83a2f983da6da19d4bee3001dd0b6a990d8d27db
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-04-16 19:37:02 +0400

    write web page from url

--- 16.04.21/src/main/java/ioread/Main.java
+++ 16.04.21/src/main/java/ioread/Main.java
@@ -1,23 +1,36 @@
 package ioread;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            InputStream inputStream = new FileInputStream("src/main/resources/somefle.txt");
+            InputStream inputStream = new FileInputStream("src/main/resources/somefile.txt");
             byte[] buffer = new byte[1024];
-            inputStream.read(buffer, 0, buffer.length);
-            String result = new String(buffer);
-            System.out.println(result);
-        } catch (IOException e) {
+            int length = 0;
+            while ((length = inputStream.read(buffer)) != -1) {
+                System.out.println(new String(buffer, 0, length));
+            }
+        }
+         catch (IOException e) {
             e.printStackTrace();
         }
 
+        try (InputStream inputStream = new URL("https://ru.wikipedia.org/wiki/%D0%9A%D0%B8%D1%82%D0%B0%D0%B9%D1%81%D0%BA%D0%B8%D0%B9_%D0%BC%D0%BE%D0%BD%D0%B0%D0%BB").openStream()){
+            OutputStream outputStream = new FileOutputStream("src/main/resources/out.txt");
+            byte[] buffer = new byte[1024];
+            int length = 0;
+            while ((length = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer);
+            }
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
