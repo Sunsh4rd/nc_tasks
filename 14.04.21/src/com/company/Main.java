fd2a9c5999b419754b409296f0be3f3a6521c3e6
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-04-18 20:37:24 +0400

    Stream api old style

--- 14.04.21/src/com/company/Main.java
+++ 14.04.21/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
 
@@ -12,9 +13,48 @@ public class Main {
             randoms[i] = (int) (Math.random() * 1000);
         }
 
-        System.out.println(Arrays.toString(boxed(randoms)));
-        System.out.println(filter(boxed(randoms)));
+        Integer[] boxed = boxed(randoms);
+        System.out.println(Arrays.toString(boxed));
+        ArrayList<Integer> filter = filter(boxed);
+        System.out.println(filter);
+        ArrayList<Integer> sorted = sorted(filter);
+        System.out.println(sorted);
+        ArrayList<Integer> skip = new ArrayList<>();
+        try {
+             skip = skip(sorted, 10);
+            System.out.println(skip);
+        } catch (Exception e) {
+            System.out.println("Wrong n");
+        }
+        ArrayList<Integer> limit = new ArrayList<>();
+        try {
+             limit = limit(skip, 10);
+            System.out.println(limit);
+        } catch (Exception e) {
+            System.out.println("Wrong n");
+        }
+        ArrayList<Integer> distinct = distinct(limit);
+        System.out.println(distinct);
+        ArrayList<Color> map = map(distinct);
+        System.out.println(map);
+        peek(map);
+        System.out.println(map);
+        ArrayList<String> mapToString;
+        mapToString = mapToString(map);
+        System.out.println(mapToString);
+        ArrayList<String[]> flatMap;
+        flatMap = flatMap(mapToString);
+        for (String[] s: flatMap) {
+            for (String str: s) {
+                System.out.println(str);
+            }
+        }
+        ArrayList<String> removeChars;
+        removeChars = removeChars(flatMap);
+        System.out.println(removeChars);
 
+        String reduce = reduce(removeChars);
+        System.out.println(reduce);
     }
 
     public static Integer[] boxed(int[] arr) {
@@ -35,6 +75,97 @@ public class Main {
             }
         }
         return filter;
+    }
+
+    public static ArrayList<Integer> sorted(ArrayList<Integer> arr) {
+        ArrayList<Integer> sorted = new ArrayList<>(arr);
+        sorted.sort(Comparator.naturalOrder());
+        return sorted;
+    }
+
+    public static ArrayList<Integer> skip(ArrayList<Integer> arr, int n) throws Exception {
+        ArrayList<Integer> skip = new ArrayList<>();
+        if (n > arr.size()) {
+            throw new Exception();
+        } else {
+            for (int i = n; i < arr.size(); i++) {
+                skip.add(arr.get(i));
+            }
+        }
+        return skip;
+    }
+
+    public static ArrayList<Integer> limit(ArrayList<Integer> arr, int n) throws Exception {
+        ArrayList<Integer> limit = new ArrayList<>();
+        if (n > arr.size()) {
+            throw new Exception();
+        } else {
+            for (int i = 0; i < n; i++) {
+                limit.add(arr.get(i));
+            }
+        }
+        return limit;
+    }
+
+    public static ArrayList<Integer> distinct(ArrayList<Integer> arr) {
+        ArrayList<Integer> distinct = new ArrayList<>();
+        for (Integer i: arr) {
+            if (!distinct.contains(i))
+            distinct.add(i);
+        }
+        return distinct;
+    }
+
+    public static ArrayList<Color> map(ArrayList<Integer> arr) {
+        ArrayList<Color> map = new ArrayList<>();
+        for (Integer i: arr) {
+            map.add(new Color(i));
+        }
+        return map;
+    }
+
+    public static void peek(ArrayList<Color> arr) {
+        for (Color c: arr) {
+            c.setName(String.valueOf(System.currentTimeMillis()));
+        }
+    }
+
+    public static ArrayList<String> mapToString(ArrayList<Color> arr) {
+        ArrayList<String> map = new ArrayList<>();
+        for (Color c: arr) {
+            map.add(c.toString());
+        }
+        return map;
+    }
+
+    public static ArrayList<String[]> flatMap(ArrayList<String> arr) {
+        ArrayList<String[]> flatMap = new ArrayList<>();
+        for (String s: arr) {
+            flatMap.add(s.split("="));
+        }
+        return flatMap;
+    }
+
+    public static ArrayList<String> removeChars(ArrayList<String[]> arg) {
+        ArrayList<String> removeChars = new ArrayList<>();
+        for (String[] s: arg) {
+            for (String str: s) {
+                String replace = str.replace("{", " ");
+                String repl2 = replace.replace(",", "");
+                String repl3 = repl2.replace("'", "");
+                String fin = repl3.replace("}", "");
+                removeChars.add(fin);
+            }
+        }
+        return removeChars;
+    }
+
+    public static String reduce(ArrayList<String> arr) {
+        StringBuilder reduce = new StringBuilder();
+        for (String str: arr) {
+            reduce.append(str).append(" ");
+        }
+        return String.valueOf(reduce);
     }
 }
 
