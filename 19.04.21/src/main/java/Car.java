663ffecc481f773641f5141f48a9a59da33fbeef
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-04-19 19:07:27 +0400

    cars with json

---
+++ 19.04.21/src/main/java/Car.java
@@ -0,0 +1,52 @@
+import java.util.List;
+
+public class Car {
+
+    private String engine;
+    private List<Integer> wheels;
+    private String body;
+    private boolean trunk;
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public List<Integer> getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(List<Integer> wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+        this.setTrunk(body.startsWith("1"));
+    }
+
+    public boolean isTrunk() {
+        return trunk;
+    }
+
+    public void setTrunk(boolean trunk) {
+        this.trunk = trunk;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "engine='" + engine + '\'' +
+                ", wheels=" + wheels +
+                ", body='" + body + '\'' +
+                ", trunk=" + trunk +
+                '}';
+    }
+}
--- 19.04.21/src/main/java/Main.java
+++ 19.04.21/src/main/java/Main.java
@@ -1,2 +1,40 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+//        try(InputStream inputStream = new FileInputStream("test.json")) {
+//            Student student = objectMapper.readValue(inputStream, Student.class);
+//            System.out.println(student);
+//        }
+
+        Car car1 = new Car();
+        car1.setEngine("Powerful engine");
+        car1.setWheels(Arrays.asList(1,1,1,1));
+        car1.setBody("2a1pij");
+        System.out.println(objectMapper.writeValueAsString(car1));
+        //{"engine":"Powerful engine","wheels":[1,1,1,1],"body":"2a1pij","trunk":false}
+        Car car2 = new Car();
+        car2.setEngine("Weak engine");
+        car2.setWheels(Arrays.asList(1,1,1,1));
+        car2.setBody("1ask2r");
+        System.out.println(objectMapper.writeValueAsString(car2));
+    }
+
+    private static void serialization() throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Student student = new Student();
+        student.setName("Vasya");
+        student.setLastName("Pupkin");
+        student.setMarks(Arrays.asList(5,5,5,4));
+        try(OutputStream outputStream = new FileOutputStream("test.json")) {
+            objectMapper.writeValue(outputStream, student);
+        }
+    }
 }
