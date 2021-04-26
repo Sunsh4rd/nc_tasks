94fb0917dca9d6568e1eb71e06d080432b32134a
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-04-26 19:14:56 +0400

    23.04

--- 23.04.21/src/main/java/Main.java
+++ 23.04.21/src/main/java/Main.java
@@ -1,11 +1,20 @@
 import stock.Stock;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
+//        File file = new File("src/main/resources");
+//        File[] files = file.listFiles();
+        stock();
+    }
+
+    private static void stock() {
         Stock stock = new Stock();
 
         for (int i = 0; i < 5; i++) {
@@ -13,6 +22,9 @@ public class Main {
                 Random random = new Random();
                 int opt = random.nextInt(3);
                 int money = random.nextInt(20);
+                System.out.println(stock.getDollarCount().get());
+                System.out.println(stock.getEuroCount().get());
+                System.out.println(stock.getRoubleCount().get());
                 switch (opt) {
                     case 0:
                         stock.exchangeED(money);
@@ -28,7 +40,7 @@ public class Main {
                 System.out.println(stock.getEuroCount().get());
                 System.out.println(stock.getRoubleCount().get());
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
