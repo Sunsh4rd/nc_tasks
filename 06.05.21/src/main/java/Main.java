03ad0805b7672761bad9c68539b85f86878874c2
Sunsh4rd <63465277+Sunsh4rd@users.noreply.github.com>
2021-05-06 19:45:32 +0400

    mysql connect

--- 06.05.21/src/main/java/Main.java
+++ 06.05.21/src/main/java/Main.java
@@ -8,14 +8,19 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        List<User> users = new ArrayList<>();
         Connection connection = getConnection();
         String query = "select * from users";
         Statement statement = connection.prepareStatement(query);
         ResultSet resultSet = statement.executeQuery(query);
 
+        while (resultSet.next())
+            users.add(new User(resultSet.getInt("id"), resultSet.getString("username")));
+
+        users.forEach(System.out::println);
     }
 
-    public static Connection getConnection() throws Exception {
+    private static Connection getConnection() throws Exception {
         DriverManager.registerDriver(new Driver());
         return DriverManager.getConnection("jdbc:mysql://localhost/newdb", "root", "VeryDifficultPass");
     }
